03837295fb8766247e6a2fa774daaeb4511a897c
Rico <rico@mymakercorner.com>
2023-10-08 14:58:39 +0200

    [bugfix] Fixed F122 firmware.

--- keyboards/leyden_jar/model_f_f122/rules.mk
+++ keyboards/leyden_jar/model_f_f122/rules.mk
@@ -7,6 +7,6 @@ EEPROM_DRIVER = wear_leveling
 WEAR_LEVELING_DRIVER = rp2040_flash
 
 SRC += keyboards/leyden_jar/matrix.c keyboards/leyden_jar/dac.c keyboards/leyden_jar/common.c keyboards/leyden_jar/pio_matrix_scan.c
-SRC += keyboards/leyden_jar/io_expander.c keyboards/leyden_jar/leds.c
+SRC += keyboards/leyden_jar/io_expander.c keyboards/leyden_jar/leds.c keyboards/leyden_jar/util_protocol.c
 QUANTUM_LIB_SRC += i2c_master.c
 
