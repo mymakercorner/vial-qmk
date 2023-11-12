f4a32e2ad36fdd5afa8ea86266dff0639c61e93c
Rico <rico@mymakercorner.com>
2023-11-12 17:06:58 +0100

    [feature] Added support of B104 v2

---
+++ keyboards/leyden_jar/b104_v2/keymaps/vial/config.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#define DYNAMIC_KEYMAP_LAYER_COUNT 3
+
+#define VIAL_KEYBOARD_UID {0xEA, 0x18, 0xE2, 0x2A, 0xFE, 0x89, 0x9E, 0xBD}
+#define VIAL_UNLOCK_COMBO_ROWS {0, 1}
+#define VIAL_UNLOCK_COMBO_COLS {0, 1}
+
+/* Esc is the bootmagic key */
+#define BOOTMAGIC_LITE_ROW 7
+#define BOOTMAGIC_LITE_COLUMN 0
+
+#define HAPTIC_OFF_IN_LOW_POWER 1
