d8b53cddd9698b36f58eebd65cc8d81bed0eaca1
Rico <rico@mymakercorner.com>
2023-11-25 17:36:03 +0100

    [feature] Added support of B122.

---
+++ keyboards/leyden_jar/b122/keymaps/vial/config.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#define DYNAMIC_KEYMAP_LAYER_COUNT 3
+
+#define VIAL_KEYBOARD_UID {0x30, 0x4D, 0x1E, 0xFD, 0xA9, 0xC1, 0x72, 0xDB}
+#define VIAL_UNLOCK_COMBO_ROWS {0, 1}
+#define VIAL_UNLOCK_COMBO_COLS {0, 1}
+
+/* Space is the bootmagic key */
+#define BOOTMAGIC_LITE_ROW 5
+#define BOOTMAGIC_LITE_COLUMN 0
+
+#define HAPTIC_OFF_IN_LOW_POWER 1
