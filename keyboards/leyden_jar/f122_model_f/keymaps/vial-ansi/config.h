1e8f03f528e2843243f2b486b9ac0dd2b4730cac
Rico <rico@mymakercorner.com>
2024-05-20 22:45:08 +0200

    [feature] Added vial-ansi and vial-iso keymaps

---
+++ keyboards/leyden_jar/f122_model_f/keymaps/vial-ansi/config.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#define DYNAMIC_KEYMAP_LAYER_COUNT 3
+
+#define VIAL_KEYBOARD_UID {0x5A, 0xB1, 0x5D, 0x5D, 0xCB, 0x88, 0x33, 0xF0}
+#define VIAL_UNLOCK_COMBO_ROWS {0, 1}
+#define VIAL_UNLOCK_COMBO_COLS {0, 1}
+
+/* Esc is the bootmagic key */
+#define BOOTMAGIC_LITE_ROW 5
+#define BOOTMAGIC_LITE_COLUMN 0
+
+#define HAPTIC_OFF_IN_LOW_POWER 1
---
+++ keyboards/leyden_jar/f122_model_f/keymaps/vial-iso/config.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#define DYNAMIC_KEYMAP_LAYER_COUNT 3
+
+#define VIAL_KEYBOARD_UID {0x36, 0x82, 0x33, 0xF6, 0xEC, 0x90, 0x47, 0x39}
+#define VIAL_UNLOCK_COMBO_ROWS {0, 1}
+#define VIAL_UNLOCK_COMBO_COLS {0, 1}
+
+/* Esc is the bootmagic key */
+#define BOOTMAGIC_LITE_ROW 5
+#define BOOTMAGIC_LITE_COLUMN 0
+
+#define HAPTIC_OFF_IN_LOW_POWER 1
