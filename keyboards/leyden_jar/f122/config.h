5a29fafa9708206ef30c7f7f8f0efcbbd106d0b3
Rico <rico@mymakercorner.com>
2024-07-03 23:44:41 +0200

    [bugfix] Put NumLock and Num / keys in a custom bin.
    Lowered activation offset.

--- keyboards/leyden_jar/f122/config.h
+++ keyboards/leyden_jar/f122/config.h
@@ -33,7 +33,12 @@
 
 #define MATRIX_FORMAT_LEYDEN_JAR
 
-#define ACTIVATION_OFFSET   14
+#define ACTIVATION_OFFSET   10
 
 #define CONTROLLER_ROWS     8
 #define CONTROLLER_COLS     18
+
+#define NB_CAL_BINS         2
+#define NB_CUSTOM_CAL_BINS  1
+
+#define CUSTOM_CAL_BIN_KEYS {{0,7,3},{0,6,3}}
