76d078f1f42b66c0464ea138ccddf0606b0b2a83
Rico <rico@mymakercorner.com>
2024-12-07 15:58:30 +0100

    [feature] Force deactivation of the boot magic lite feature.

--- keyboards/leyden_jar/matrix.c
+++ keyboards/leyden_jar/matrix.c
@@ -49,3 +49,20 @@ bool matrix_scan_custom(matrix_row_t current_matrix[]) {
 
     return matrix_has_changed;
 }
+
+/*
+ * This empty function disables bootmagic lite feature, were plugging in the keyboard while a particuliar key is pressed
+ * allows the MCU to go to the bootloader.
+ * We do that because when VIA/Vial features are activated this also activate the bootmagic lite feature, even if the
+ * value of "bootmagic" field in info.json has the value false.
+ *
+ * Doing this is particuliarly critical on beam spring boards where an unpopulated pad (or a malfunctioning key) triggers a keypress.
+ * It can be quite confusing to try flashing a new firmware and have the keyboard go directly into the bootloader after doing so.
+ * As this exact situation occured to Ellipse and me (and was quite hard to figure out) it has been decided to deactivate the feature.
+ *
+ * There are still several other ways to go the bootloader:
+ * - At any time use the key combination configured in the keymap.
+ * - Use the Leyden Jar Diagnostic Tool.
+ */
+
+void bootmagic_lite(void){}
