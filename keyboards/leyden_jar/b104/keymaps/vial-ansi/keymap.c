9b43fea2836afe37b4c6df133c1bdc3fc5746d68
Rico <rico@mymakercorner.com>
2024-12-16 22:46:08 +0100

    [feature] Re-enabled EEPROM clear and QMK reboot in layer 2 keymap for B104.

--- keyboards/leyden_jar/b104/keymaps/vial-ansi/keymap.c
+++ keyboards/leyden_jar/b104/keymaps/vial-ansi/keymap.c
@@ -45,7 +45,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_FN2] = LAYOUT(
         KC_NO,        KC_NO, KC_NO, KC_NO, KC_NO,    KC_NO, KC_NO, KC_NO, KC_NO,     KC_NO, KC_NO, KC_NO, KC_NO,            KC_NO, KC_NO, KC_NO,
         KC_NO, KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO, KC_NO, KC_NO, KC_NO,  KC_NO, KC_NO, KC_NO,    KC_NO, KC_NO, KC_NO, KC_NO,
-        KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,                   KC_NO, KC_NO, KC_NO,    KC_NO, KC_NO, KC_NO, KC_NO,
+        KC_NO, KC_NO, KC_NO, EE_CLR, QK_BOOT, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,                KC_NO, KC_NO, KC_NO,    KC_NO, KC_NO, KC_NO, KC_NO,
         KC_NO,     KC_NO, KC_NO, KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO, KC_NO,KC_NO, KC_NO,         KC_NO, KC_NO, KC_NO,    KC_NO, KC_NO, KC_NO,
         KC_NO,  KC_NO, KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,    KC_NO, KC_NO, KC_NO,    KC_NO, KC_NO, KC_NO, KC_NO,
         KC_NO, KC_NO, KC_NO,                MO(_FN2),                       KC_NO, KC_NO, KC_NO, KC_NO,                     KC_NO, KC_NO, KC_NO,    KC_NO, KC_NO, KC_NO
