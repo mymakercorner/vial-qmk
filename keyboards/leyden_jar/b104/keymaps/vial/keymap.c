e174fabc58aacd5662a4e9cee7fac5ef1dbde71a
Rico <rico@mymakercorner.com>
2025-02-07 21:09:57 +0100

    [cleanup] Added console feature and keymap rework for b104.

--- keyboards/leyden_jar/b104/keymaps/vial/keymap.c
+++ keyboards/leyden_jar/b104/keymaps/vial/keymap.c
@@ -31,23 +31,23 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,    KC_Q,  KC_W,  KC_E,  KC_R,  KC_T,  KC_Y,  KC_U,  KC_I,  KC_O,  KC_P,  KC_LBRC, KC_RBRC,  KC_BSLS,        KC_DEL,  KC_END,  KC_PGDN,    KC_P7,   KC_P8,   KC_P9,   KC_PPLS,
         KC_CAPS,      KC_A,  KC_S,  KC_D,  KC_F,  KC_G,  KC_H,  KC_J,  KC_K,  KC_L,  KC_SCLN, KC_QUOT,KC_ENT, KC_NO,        KC_NO,   KC_NO,   KC_NO,      KC_P4,   KC_P5,   KC_P6,
         KC_NO,    KC_LSFT, KC_Z,  KC_X,  KC_C,  KC_V,  KC_B,  KC_N,  KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_NO, KC_RSFT, KC_NO, KC_NO,   KC_UP,   KC_NO,      KC_P1,   KC_P2,   KC_P3,   KC_PENT,
-        KC_LCTL,  KC_LGUI, KC_LALT,                KC_SPC,                       KC_RALT, KC_RGUI, KC_APP, MO(_FN1),        KC_LEFT, KC_DOWN, KC_RGHT,    KC_P0,   KC_NO,   KC_PDOT
+        KC_LCTL,  KC_LGUI, KC_LALT,                KC_SPC,                       KC_RALT,   MO(_FN1), KC_NO, KC_RCTL,       KC_LEFT, KC_DOWN, KC_RGHT,    KC_P0,   KC_NO,   KC_PDOT
     ),
 
     [_FN1] = LAYOUT(
-        KC_NO,        KC_NO, KC_NO, KC_NO, KC_NO,    KC_NO, KC_NO, KC_NO, KC_NO,     KC_NO, KC_NO, KC_NO, KC_NO,            KC_NO, KC_NO, KC_NO,
-        KC_NO, KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO, KC_NO, KC_NO, KC_NO,  KC_NO, KC_NO, KC_NO,     KC_NO, KC_NO, KC_NO, KC_NO,
-        KC_NO, HF_ON, HF_OFF, HF_TOGG, HF_RST, HF_DWLD, HF_DWLU, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,           KC_NO, KC_NO, KC_NO,     KC_NO, KC_NO, KC_NO, KC_NO,
-        KC_NO,     KC_VOLD, KC_VOLU, KC_MUTE,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO, KC_NO,KC_NO, KC_NO,   KC_NO, KC_NO, KC_NO,     KC_NO, KC_NO, KC_NO,
-        KC_NO,  KC_NO, KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,    KC_NO, KC_NO, KC_NO,     KC_NO, KC_NO, KC_NO, KC_NO,
-        KC_NO, KC_NO, KC_NO,                MO(_FN2),                       KC_NO, KC_NO, KC_NO, KC_NO,                     KC_NO, KC_NO, KC_NO,     KC_NO, KC_NO, KC_NO
+        KC_PWR, KC_BRID, KC_BRIU, KC_NO,  KC_NO,  KC_NO,  KC_MSTP, KC_MPRV, KC_MPLY,  KC_MNXT, KC_MUTE, KC_VOLD, KC_VOLU,   KC_EJCT, KC_NO, KC_NO,
+        KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,   KC_NO, KC_NO,     KC_NO, KC_NO, KC_NO, KC_NO,
+        KC_NO,  KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,        KC_NO,    KC_NO,   KC_NO, KC_NO,     KC_NO, KC_NO, KC_NO,
+        KC_NO,    KC_NO, KC_NO, KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO, KC_NO,KC_NO, KC_NO,          KC_NO,   KC_NO, KC_NO,     KC_NO, KC_NO, KC_NO,
+        KC_NO,  KC_NO, KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,    KC_NO,   KC_NO, KC_NO,     KC_NO, KC_NO, KC_NO, KC_NO,
+        KC_NO, KC_NO, KC_NO,                MO(_FN2),                       KC_NO,  KC_TRNS, KC_NO,   KC_NO,                KC_NO,   KC_NO, KC_NO,     KC_NO, KC_NO, KC_NO
     ),
     [_FN2] = LAYOUT(
-        KC_NO,        KC_NO, KC_NO, KC_NO, KC_NO,    KC_NO, KC_NO, KC_NO, KC_NO,     KC_NO, KC_NO, KC_NO, KC_NO,            KC_NO, KC_NO, KC_NO,
-        KC_NO, KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO, KC_NO, KC_NO, KC_NO,  KC_NO, KC_NO, KC_NO,    KC_NO, KC_NO, KC_NO, KC_NO,
-        KC_NO, KC_NO, KC_NO, EE_CLR, QK_BOOT, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,                KC_NO, KC_NO, KC_NO,    KC_NO, KC_NO, KC_NO, KC_NO,
-        KC_NO,     KC_NO, KC_NO, KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO, KC_NO,KC_NO, KC_NO,         KC_NO, KC_NO, KC_NO,    KC_NO, KC_NO, KC_NO,
-        KC_NO,  KC_NO, KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,    KC_NO, KC_NO, KC_NO,    KC_NO, KC_NO, KC_NO, KC_NO,
-        KC_NO, KC_NO, KC_NO,                MO(_FN2),                       KC_NO, KC_NO, KC_NO, KC_NO,                     KC_NO, KC_NO, KC_NO,    KC_NO, KC_NO, KC_NO
+        KC_NO,        KC_NO, KC_NO, KC_NO, KC_NO,    KC_NO, KC_NO, KC_NO, KC_NO,     KC_NO, KC_NO, KC_NO, KC_NO,            KC_NO,   KC_NO, KC_NO,
+        KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  HF_DWLD, HF_DWLU, KC_NO, KC_NO,  KC_NO,   KC_NO, KC_NO,    KC_NO, KC_NO, KC_NO, KC_NO,
+        KC_NO, KC_NO, KC_NO, EE_CLR, QK_BOOT, HF_TOGG, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,              KC_NO,   KC_NO, KC_NO,    KC_NO, KC_NO, KC_NO, KC_NO,
+        KC_NO,     KC_NO, KC_NO, KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO, KC_NO,KC_NO, KC_NO,         KC_NO,   KC_NO, KC_NO,    KC_NO, KC_NO, KC_NO,
+        KC_NO,  KC_NO, KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,    KC_NO,   KC_NO, KC_NO,    KC_NO, KC_NO, KC_NO, KC_NO,
+        KC_NO, KC_NO, KC_NO,                KC_TRNS,                       KC_NO, KC_TRNS, KC_NO, KC_NO,                    KC_NO,   KC_NO, KC_NO,    KC_NO, KC_NO, KC_NO
     )
 };
