8d5ca1fc2ebe22582242ebd97b31fe1478186e69
Rico <rico@mymakercorner.com>
2025-05-20 19:40:28 +0200

    [feature] Forgot to push the keymaps and nkro settings.

--- keyboards/leyden_jar/b104/keymaps/vial/keymap.c
+++ keyboards/leyden_jar/b104/keymaps/vial/keymap.c
@@ -31,7 +31,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,    KC_Q,  KC_W,  KC_E,  KC_R,  KC_T,  KC_Y,  KC_U,  KC_I,  KC_O,  KC_P,  KC_LBRC, KC_RBRC,  KC_BSLS,        KC_DEL,  KC_END,  KC_PGDN,    KC_P7,   KC_P8,   KC_P9,   KC_PPLS,
         KC_CAPS,      KC_A,  KC_S,  KC_D,  KC_F,  KC_G,  KC_H,  KC_J,  KC_K,  KC_L,  KC_SCLN, KC_QUOT,KC_ENT, KC_NO,        KC_NO,   KC_NO,   KC_NO,      KC_P4,   KC_P5,   KC_P6,
         KC_NO,    KC_LSFT, KC_Z,  KC_X,  KC_C,  KC_V,  KC_B,  KC_N,  KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_NO, KC_RSFT, KC_NO, KC_NO,   KC_UP,   KC_NO,      KC_P1,   KC_P2,   KC_P3,   KC_PENT,
-        KC_LCTL,  KC_LGUI, KC_LALT,                KC_SPC,                       KC_RALT,   MO(_FN1), KC_NO, KC_RCTL,       KC_LEFT, KC_DOWN, KC_RGHT,    KC_P0,   KC_NO,   KC_PDOT
+        KC_LCTL,  KC_LGUI, KC_LALT,                KC_SPC,                       KC_RALT,   MO(_FN1), KC_APP, KC_RCTL,      KC_LEFT, KC_DOWN, KC_RGHT,    KC_P0,   KC_NO,   KC_PDOT
     ),
 
     [_FN1] = LAYOUT(
@@ -47,7 +47,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  HF_DWLD, HF_DWLU, KC_NO, KC_NO,  KC_NO,   KC_NO, KC_NO,    KC_NO, KC_NO, KC_NO, KC_NO,
         KC_NO, KC_NO, KC_NO, EE_CLR, QK_BOOT, HF_TOGG, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,              KC_NO,   KC_NO, KC_NO,    KC_NO, KC_NO, KC_NO, KC_NO,
         KC_NO,     KC_NO, KC_NO, KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO, KC_NO,KC_NO, KC_NO,         KC_NO,   KC_NO, KC_NO,    KC_NO, KC_NO, KC_NO,
-        KC_NO,  KC_NO, KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,    KC_NO,   KC_NO, KC_NO,    KC_NO, KC_NO, KC_NO, KC_NO,
+        KC_NO,  KC_NO, KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  NK_TOGG, KC_NO,  KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,   KC_NO,   KC_NO, KC_NO,    KC_NO, KC_NO, KC_NO, KC_NO,
         KC_NO, KC_NO, KC_NO,                KC_TRNS,                       KC_NO, KC_TRNS, KC_NO, KC_NO,                    KC_NO,   KC_NO, KC_NO,    KC_NO, KC_NO, KC_NO
     )
 };
