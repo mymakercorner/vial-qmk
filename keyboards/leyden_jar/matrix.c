11a831e29a45a1a850c9c3b38bfefed3f04e5bca
Rico <rico@mymakercorner.com>
2025-05-28 22:57:04 +0200

    [feature] Wrote a custom left/right part detection that overrides the default QMK implementation.

--- keyboards/leyden_jar/matrix.c
+++ keyboards/leyden_jar/matrix.c
@@ -72,3 +72,44 @@ bool matrix_scan_custom(matrix_row_t current_matrix[]) {
  */
 
 void bootmagic_lite(void){}
+
+/*
+ * This function overrides the default QMK implementation of left/right keyboard part detection in case of split keyboards.
+ * The default implementation is in file 'quantum/split_util.c' declared as weak linking attribute to be able to be overriden on specific keyboards.
+ * There is also a bit of simplification in the code done by removing cases scenario that we don't want covered in the Leyden Jar.
+ *
+ * We removed the SPLIT_HAND_MATRIX_GRID scenario: we cannot do this implementation as the keyboard PCBs are capacitive and also we don't have control on how the PCB is designed.
+ *
+ * We also removed the EE_HANDS scenario: using this feature looks convoluted emough on AVR CPUs to discourage average users and it looks even worse when using RP2040 MCUs.
+ * Really removing the possibility to drag and drop uf2 files in the RP2040 boot disk folder and replacing it by complex command line commands is not at all user friendly.
+ *
+ * The scenarios that are still managed and available by this function are:
+ *
+ * 1) Decide at compile time if left or right part is the master (the master must be connected to the PC by USB).
+ *
+ * 2) Detect the left or right part by reading an IO pin and taking the decision based on the read level (low or high).
+ * In this case either the left or right part can be the master (that is connected to the PC by USB).
+ * This is the scenario where this function override really does its job.
+ * We'd like to use one of the extra columns IO pins GP26/GP27 for this but we dont want to change how those IO pins are wired controller side.
+ * So it is difficult for us to drive this IO pin high if wanted.
+ * Default implentation declares this IO pin with no internal pullups or pulldown resitors enabled.
+ * On our implementation we declare this IO pin as input with internal pullup activated so by default the IO pin is in a high state.
+ * Pulling this IO pin low is then very simple: just connect IO pin hole to ground, this can be done fron the keyboard PCB for example.
+ */
+
+bool is_keyboard_left_impl(void) {
+#if defined(SPLIT_HAND_PIN)
+    setPinInputHigh(SPLIT_HAND_PIN);
+    wait_us(100);
+    // Test pin SPLIT_HAND_PIN for High/Low, if low it's right hand
+#    ifdef SPLIT_HAND_PIN_LOW_IS_LEFT
+    return !readPin(SPLIT_HAND_PIN);
+#    else
+    return readPin(SPLIT_HAND_PIN);
+#    endif
+#elif defined(MASTER_RIGHT)
+    return !is_keyboard_master();
+#else
+    return is_keyboard_master();
+#endif
+}
