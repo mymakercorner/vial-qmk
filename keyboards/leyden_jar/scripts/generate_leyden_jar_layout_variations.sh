babfda114964e8c01589812916a927a8eebcf395
Rico <rico@mymakercorner.com>
2025-11-02 22:00:59 +0100

    [feature] Added script to help automate firmware layout generation.
    For F122 keyboards only at the moment.

---
+++ keyboards/leyden_jar/scripts/generate_leyden_jar_layout_variations.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+cd ./keyboards/leyden_jar/scripts
+
+python ./via_set_default_layout.py ../f122/keymaps/vial/vial.json ../f122/keymaps/vial-ansi/default_layout.json ../f122/keymaps/vial-ansi/vial.json
+python ./via_set_default_layout.py ../f122/keymaps/vial/vial.json ../f122/keymaps/vial-ansi-hhkb-split-rshift/default_layout.json ../f122/keymaps/vial-ansi-hhkb-split-rshift/vial.json
+python ./via_set_default_layout.py ../f122/keymaps/vial/vial.json ../f122/keymaps/vial-ansi-hhkb-split-rshift-lctrl-caps-revert/default_layout.json ../f122/keymaps/vial-ansi-hhkb-split-rshift-lctrl-caps-revert/vial.json
+python ./via_set_default_layout.py ../f122/keymaps/vial/vial.json ../f122/keymaps/vial-ansi-hhkb-split-rshift-split-backspace/default_layout.json ../f122/keymaps/vial-ansi-hhkb-split-rshift-split-backspace/vial.json
+python ./via_set_default_layout.py ../f122/keymaps/vial/vial.json ../f122/keymaps/vial-ansi-hhkb-split-rshift-split-backspace-lctrl-caps-revert/default_layout.json ../f122/keymaps/vial-ansi-hhkb-split-rshift-split-backspace-lctrl-caps-revert/vial.json
+python ./via_set_default_layout.py ../f122/keymaps/vial/vial.json ../f122/keymaps/vial-iso/default_layout.json ../f122/keymaps/vial-iso/vial.json
+python ./via_set_default_layout.py ../f122/keymaps/vial/vial.json ../f122/keymaps/vial-iso-hhkb-split-rshift/default_layout.json ../f122/keymaps/vial-iso-hhkb-split-rshift/vial.json
+python ./via_set_default_layout.py ../f122/keymaps/vial/vial.json ../f122/keymaps/vial-iso-hhkb-split-rshift-lctrl-caps-revert/default_layout.json ../f122/keymaps/vial-iso-hhkb-split-rshift-lctrl-caps-revert/vial.json
+python ./via_set_default_layout.py ../f122/keymaps/vial/vial.json ../f122/keymaps/vial-iso-hhkb-split-rshift-split-backspace/default_layout.json ../f122/keymaps/vial-iso-hhkb-split-rshift-split-backspace/vial.json
+python ./via_set_default_layout.py ../f122/keymaps/vial/vial.json ../f122/keymaps/vial-iso-hhkb-split-rshift-split-backspace-lctrl-caps-revert/default_layout.json ../f122/keymaps/vial-iso-hhkb-split-rshift-split-backspace-lctrl-caps-revert/vial.json
