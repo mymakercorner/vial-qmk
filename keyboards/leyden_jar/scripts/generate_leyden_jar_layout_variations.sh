c6d794c456d457531e807ad31bc319f2424f079e
Rico <rico@mymakercorner.com>
2025-11-03 22:24:49 +0100

    [feature] New specialized firmware layout feature, now for B122 and B104 keyboards.

--- keyboards/leyden_jar/scripts/generate_leyden_jar_layout_variations.sh
+++ keyboards/leyden_jar/scripts/generate_leyden_jar_layout_variations.sh
@@ -12,3 +12,9 @@ python ./via_set_default_layout.py ../f122/keymaps/vial/vial.json ../f122/keymap
 python ./via_set_default_layout.py ../f122/keymaps/vial/vial.json ../f122/keymaps/vial-iso-hhkb-split-rshift-lctrl-caps-revert/default_layout.json ../f122/keymaps/vial-iso-hhkb-split-rshift-lctrl-caps-revert/vial.json
 python ./via_set_default_layout.py ../f122/keymaps/vial/vial.json ../f122/keymaps/vial-iso-hhkb-split-rshift-split-backspace/default_layout.json ../f122/keymaps/vial-iso-hhkb-split-rshift-split-backspace/vial.json
 python ./via_set_default_layout.py ../f122/keymaps/vial/vial.json ../f122/keymaps/vial-iso-hhkb-split-rshift-split-backspace-lctrl-caps-revert/default_layout.json ../f122/keymaps/vial-iso-hhkb-split-rshift-split-backspace-lctrl-caps-revert/vial.json
+
+python ./via_set_default_layout.py ../b104/keymaps/vial/vial.json ../b104/keymaps/vial-ansi/default_layout.json ../b104/keymaps/vial-ansi/vial.json
+python ./via_set_default_layout.py ../b104/keymaps/vial/vial.json ../b104/keymaps/vial-iso/default_layout.json ../b104/keymaps/vial-iso/vial.json
+
+python ./via_set_default_layout.py ../b122/keymaps/vial/vial.json ../b122/keymaps/vial-ansi/default_layout.json ../b122/keymaps/vial-ansi/vial.json
+python ./via_set_default_layout.py ../b122/keymaps/vial/vial.json ../b122/keymaps/vial-iso/default_layout.json ../b122/keymaps/vial-iso/vial.json
