2762aa319ecea58b1a77f5f0156c000b4a27d300
Rico <rico@mymakercorner.com>
2025-11-04 20:26:27 +0100

    [cleanup] Renamed hhkb style control caps firmware variants of F122 keyboard.

--- keyboards/leyden_jar/scripts/build_leyden_jar_firmwares.sh
+++ keyboards/leyden_jar/scripts/build_leyden_jar_firmwares.sh
@@ -3,14 +3,14 @@
 qmk compile -kb leyden_jar/f122 -km vial
 qmk compile -kb leyden_jar/f122 -km vial-ansi
 qmk compile -kb leyden_jar/f122 -km vial-ansi-hhkb-split-rshift
-qmk compile -kb leyden_jar/f122 -km vial-ansi-hhkb-split-rshift-lctrl-caps-revert
+qmk compile -kb leyden_jar/f122 -km vial-ansi-hhkb-split-rshift-hhkb-style-ctrl-caps
 qmk compile -kb leyden_jar/f122 -km vial-ansi-hhkb-split-rshift-split-backspace
-qmk compile -kb leyden_jar/f122 -km vial-ansi-hhkb-split-rshift-split-backspace-lctrl-caps-revert
+qmk compile -kb leyden_jar/f122 -km vial-ansi-hhkb-split-rshift-split-backspace-hhkb-style-ctrl-caps
 qmk compile -kb leyden_jar/f122 -km vial-iso
 qmk compile -kb leyden_jar/f122 -km vial-iso-hhkb-split-rshift
-qmk compile -kb leyden_jar/f122 -km vial-iso-hhkb-split-rshift-lctrl-caps-revert
+qmk compile -kb leyden_jar/f122 -km vial-iso-hhkb-split-rshift-hhkb-style-ctrl-caps
 qmk compile -kb leyden_jar/f122 -km vial-iso-hhkb-split-rshift-split-backspace
-qmk compile -kb leyden_jar/f122 -km vial-iso-hhkb-split-rshift-split-backspace-lctrl-caps-revert
+qmk compile -kb leyden_jar/f122 -km vial-iso-hhkb-split-rshift-split-backspace-hhkb-style-ctrl-caps
 
 qmk compile -kb leyden_jar/b122 -km vial
 qmk compile -kb leyden_jar/b122 -km vial-ansi
--- keyboards/leyden_jar/scripts/generate_leyden_jar_layout_variations.sh
+++ keyboards/leyden_jar/scripts/generate_leyden_jar_layout_variations.sh
@@ -4,14 +4,14 @@ cd ./keyboards/leyden_jar/scripts
 
 python ./via_set_default_layout.py ../f122/keymaps/vial/vial.json ../f122/keymaps/vial-ansi/default_layout.json ../f122/keymaps/vial-ansi/vial.json
 python ./via_set_default_layout.py ../f122/keymaps/vial/vial.json ../f122/keymaps/vial-ansi-hhkb-split-rshift/default_layout.json ../f122/keymaps/vial-ansi-hhkb-split-rshift/vial.json
-python ./via_set_default_layout.py ../f122/keymaps/vial/vial.json ../f122/keymaps/vial-ansi-hhkb-split-rshift-lctrl-caps-revert/default_layout.json ../f122/keymaps/vial-ansi-hhkb-split-rshift-lctrl-caps-revert/vial.json
+python ./via_set_default_layout.py ../f122/keymaps/vial/vial.json ../f122/keymaps/vial-ansi-hhkb-split-rshift-hhkb-style-ctrl-caps/default_layout.json ../f122/keymaps/vial-ansi-hhkb-split-rshift-hhkb-style-ctrl-caps/vial.json
 python ./via_set_default_layout.py ../f122/keymaps/vial/vial.json ../f122/keymaps/vial-ansi-hhkb-split-rshift-split-backspace/default_layout.json ../f122/keymaps/vial-ansi-hhkb-split-rshift-split-backspace/vial.json
-python ./via_set_default_layout.py ../f122/keymaps/vial/vial.json ../f122/keymaps/vial-ansi-hhkb-split-rshift-split-backspace-lctrl-caps-revert/default_layout.json ../f122/keymaps/vial-ansi-hhkb-split-rshift-split-backspace-lctrl-caps-revert/vial.json
+python ./via_set_default_layout.py ../f122/keymaps/vial/vial.json ../f122/keymaps/vial-ansi-hhkb-split-rshift-split-backspace-hhkb-style-ctrl-caps/default_layout.json ../f122/keymaps/vial-ansi-hhkb-split-rshift-split-backspace-hhkb-style-ctrl-caps/vial.json
 python ./via_set_default_layout.py ../f122/keymaps/vial/vial.json ../f122/keymaps/vial-iso/default_layout.json ../f122/keymaps/vial-iso/vial.json
 python ./via_set_default_layout.py ../f122/keymaps/vial/vial.json ../f122/keymaps/vial-iso-hhkb-split-rshift/default_layout.json ../f122/keymaps/vial-iso-hhkb-split-rshift/vial.json
-python ./via_set_default_layout.py ../f122/keymaps/vial/vial.json ../f122/keymaps/vial-iso-hhkb-split-rshift-lctrl-caps-revert/default_layout.json ../f122/keymaps/vial-iso-hhkb-split-rshift-lctrl-caps-revert/vial.json
+python ./via_set_default_layout.py ../f122/keymaps/vial/vial.json ../f122/keymaps/vial-iso-hhkb-split-rshift-hhkb-style-ctrl-caps/default_layout.json ../f122/keymaps/vial-iso-hhkb-split-rshift-hhkb-style-ctrl-caps/vial.json
 python ./via_set_default_layout.py ../f122/keymaps/vial/vial.json ../f122/keymaps/vial-iso-hhkb-split-rshift-split-backspace/default_layout.json ../f122/keymaps/vial-iso-hhkb-split-rshift-split-backspace/vial.json
-python ./via_set_default_layout.py ../f122/keymaps/vial/vial.json ../f122/keymaps/vial-iso-hhkb-split-rshift-split-backspace-lctrl-caps-revert/default_layout.json ../f122/keymaps/vial-iso-hhkb-split-rshift-split-backspace-lctrl-caps-revert/vial.json
+python ./via_set_default_layout.py ../f122/keymaps/vial/vial.json ../f122/keymaps/vial-iso-hhkb-split-rshift-split-backspace-hhkb-style-ctrl-caps/default_layout.json ../f122/keymaps/vial-iso-hhkb-split-rshift-split-backspace-hhkb-style-ctrl-caps/vial.json
 
 python ./via_set_default_layout.py ../b104/keymaps/vial/vial.json ../b104/keymaps/vial-ansi/default_layout.json ../b104/keymaps/vial-ansi/vial.json
 python ./via_set_default_layout.py ../b104/keymaps/vial/vial.json ../b104/keymaps/vial-iso/default_layout.json ../b104/keymaps/vial-iso/vial.json
