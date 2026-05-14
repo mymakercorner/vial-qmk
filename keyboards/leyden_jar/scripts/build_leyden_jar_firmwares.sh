a13d548f7a7ebc8bc6e2d5d5ae38656c55a8aa96
Rico <rico@mymakercorner.com>
2026-05-14 19:18:45 +0200

    [bugfix] Fixed ISO layouts on B122, B104 and F122 keyboards.
    Also changed to shorter names for hhkb style ctrl/caps placement layouts.

--- keyboards/leyden_jar/scripts/build_leyden_jar_firmwares.sh
+++ keyboards/leyden_jar/scripts/build_leyden_jar_firmwares.sh
@@ -3,14 +3,14 @@
 qmk compile -kb leyden_jar/f122 -km vial
 qmk compile -kb leyden_jar/f122 -km vial-ansi
 qmk compile -kb leyden_jar/f122 -km vial-ansi-hhkb-split-rshift
-qmk compile -kb leyden_jar/f122 -km vial-ansi-hhkb-split-rshift-hhkb-style-ctrl-caps
+qmk compile -kb leyden_jar/f122 -km vial-ansi-hhkb-split-rshift-hhkb-ctrl-caps
 qmk compile -kb leyden_jar/f122 -km vial-ansi-hhkb-split-rshift-split-backspace
-qmk compile -kb leyden_jar/f122 -km vial-ansi-hhkb-split-rshift-split-backspace-hhkb-style-ctrl-caps
+qmk compile -kb leyden_jar/f122 -km vial-ansi-hhkb-split-rshift-split-backspace-hhkb-ctrl-caps
 qmk compile -kb leyden_jar/f122 -km vial-iso
 qmk compile -kb leyden_jar/f122 -km vial-iso-hhkb-split-rshift
-qmk compile -kb leyden_jar/f122 -km vial-iso-hhkb-split-rshift-hhkb-style-ctrl-caps
+qmk compile -kb leyden_jar/f122 -km vial-iso-hhkb-split-rshift-hhkb-ctrl-caps
 qmk compile -kb leyden_jar/f122 -km vial-iso-hhkb-split-rshift-split-backspace
-qmk compile -kb leyden_jar/f122 -km vial-iso-hhkb-split-rshift-split-backspace-hhkb-style-ctrl-caps
+qmk compile -kb leyden_jar/f122 -km vial-iso-hhkb-split-rshift-split-backspace-hhkb-ctrl-caps
 
 qmk compile -kb leyden_jar/b122 -km vial
 qmk compile -kb leyden_jar/b122 -km vial-ansi
