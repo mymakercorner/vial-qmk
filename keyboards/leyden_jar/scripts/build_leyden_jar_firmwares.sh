dd0f5091761f0643ebc0c91eb41548ba429b5083
Rico <rico@mymakercorner.com>
2026-06-06 11:57:20 +0200

    [opti] Launch several firmware compilation processes in parallel.
    This allows for a massive gain of compilation time.
    F50 keyboard has been added to the list as well.

--- keyboards/leyden_jar/scripts/build_leyden_jar_firmwares.sh
+++ keyboards/leyden_jar/scripts/build_leyden_jar_firmwares.sh
@@ -1,27 +1,33 @@
 #!/usr/bin/env bash
 
-qmk compile -kb leyden_jar/f122 -km vial
-qmk compile -kb leyden_jar/f122 -km vial-ansi
-qmk compile -kb leyden_jar/f122 -km vial-ansi-hhkb-split-rshift
-qmk compile -kb leyden_jar/f122 -km vial-ansi-hhkb-split-rshift-hhkb-ctrl-caps
-qmk compile -kb leyden_jar/f122 -km vial-ansi-hhkb-split-rshift-split-backspace
-qmk compile -kb leyden_jar/f122 -km vial-ansi-hhkb-split-rshift-split-backspace-hhkb-ctrl-caps
-qmk compile -kb leyden_jar/f122 -km vial-iso
-qmk compile -kb leyden_jar/f122 -km vial-iso-hhkb-split-rshift
-qmk compile -kb leyden_jar/f122 -km vial-iso-hhkb-split-rshift-hhkb-ctrl-caps
-qmk compile -kb leyden_jar/f122 -km vial-iso-hhkb-split-rshift-split-backspace
-qmk compile -kb leyden_jar/f122 -km vial-iso-hhkb-split-rshift-split-backspace-hhkb-ctrl-caps
+qmk compile -kb leyden_jar/f122 -km vial &
+qmk compile -kb leyden_jar/f122 -km vial-ansi &
+qmk compile -kb leyden_jar/f122 -km vial-ansi-hhkb-split-rshift &
+qmk compile -kb leyden_jar/f122 -km vial-ansi-hhkb-split-rshift-hhkb-ctrl-caps &
+qmk compile -kb leyden_jar/f122 -km vial-ansi-hhkb-split-rshift-split-backspace &
+qmk compile -kb leyden_jar/f122 -km vial-ansi-hhkb-split-rshift-split-backspace-hhkb-ctrl-caps &
+qmk compile -kb leyden_jar/f122 -km vial-iso &
+qmk compile -kb leyden_jar/f122 -km vial-iso-hhkb-split-rshift &
+qmk compile -kb leyden_jar/f122 -km vial-iso-hhkb-split-rshift-hhkb-ctrl-caps &
+qmk compile -kb leyden_jar/f122 -km vial-iso-hhkb-split-rshift-split-backspace &
+qmk compile -kb leyden_jar/f122 -km vial-iso-hhkb-split-rshift-split-backspace-hhkb-ctrl-caps &
 
-qmk compile -kb leyden_jar/b122 -km vial
-qmk compile -kb leyden_jar/b122 -km vial-ansi
-qmk compile -kb leyden_jar/b122 -km vial-iso
+wait
 
-qmk compile -kb leyden_jar/b104 -km vial
-qmk compile -kb leyden_jar/b104 -km vial-ansi
-qmk compile -kb leyden_jar/b104 -km vial-iso
+qmk compile -kb leyden_jar/b122 -km vial &
+qmk compile -kb leyden_jar/b122 -km vial-ansi &
+qmk compile -kb leyden_jar/b122 -km vial-iso &
 
-qmk compile -kb leyden_jar/f104 -km vial
+qmk compile -kb leyden_jar/b104 -km vial &
+qmk compile -kb leyden_jar/b104 -km vial-ansi &
+qmk compile -kb leyden_jar/b104 -km vial-iso &
 
-qmk compile -kb leyden_jar/f77 -km vial
+qmk compile -kb leyden_jar/f104 -km vial &
 
-qmk compile -kb leyden_jar/f62 -km vial
+qmk compile -kb leyden_jar/f77 -km vial &
+
+qmk compile -kb leyden_jar/f62 -km vial &
+
+qmk compile -kb leyden_jar/f50 -km vial &
+
+wait
